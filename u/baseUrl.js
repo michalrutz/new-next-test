47110442bf624f712af050a95475c73a53b3453d
michalrutz <michal.rutz@gmail.com>
2020-05-20 22:02:47 +0200

    corrected url!

--- u/baseUrl.js
+++ u/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://michal-test.herokuapp.com.herokuapp.com"
+    ? "https://michal-test.herokuapp.com"
     : "http://localhost:3000";
 
 export default baseUrl;
